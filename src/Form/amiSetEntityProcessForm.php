b2d09a0c85b306beaae74f7b8e5d9c6fa6980360
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2022-06-06 16:32:13 -0600

    Remove left over echo while testig

--- src/Form/amiSetEntityProcessForm.php
+++ src/Form/amiSetEntityProcessForm.php
@@ -285,7 +285,6 @@ class amiSetEntityProcessForm extends ContentEntityConfirmFormBase {
       $si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB' );
       $base = 1024;
       $class = min((int)log($bytes , $base) , count($si_prefix) - 1);
-      echo sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
 
       $form['status'] = [
         '#tree' => TRUE,
