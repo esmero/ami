ff97f7c1aa114c415c57eeb49f5a1421d011af08
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2024-05-02 18:22:13 -0400

    Old comment. Just a comment but Spring clean ups are kay Diego

--- src/Plugin/ImporterAdapter/SolrImporter.php
+++ src/Plugin/ImporterAdapter/SolrImporter.php
@@ -66,7 +66,7 @@ class SolrImporter extends SpreadsheetImporter {
   protected $httpClient;
 
   /**
-   * GoogleSheetImporter constructor.
+   * Solr Importer constructor.
    *
    * @param array $configuration
    * @param $plugin_id
